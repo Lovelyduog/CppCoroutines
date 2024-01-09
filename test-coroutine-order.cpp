355989c95904e1d8a9f5832eca161bd1fb34d0d8
shenglish <2285575104@qq.com>
2024-01-10 00:30:02 +0800

    新的问题

--- test-coroutine-order.cpp
+++ test-coroutine-order.cpp
@@ -2,7 +2,7 @@
 #include <future>
 #include <chrono>
 #include <iostream>
-
+#include <optional>
 // 前置类型声明
 template <typename ReturnType>
 struct CoroutineTask;
@@ -20,18 +20,30 @@ struct initial_suspend_awaiter
     constexpr bool await_ready() const noexcept { return false; }
     //  constexpr bool await_ready() const noexcept { return true; }
 
-    constexpr void await_suspend(std::coroutine_handle< > h)  {
-        // std::async([=](){
-        //     std::this_thread::sleep_for(std::chrono::seconds(2));
-        //     h.resume();
-        // });
+    constexpr void await_suspend(std::coroutine_handle<> &&h)  {
         h_ = h;
+    //    std::async([=](){
+    //         std::this_thread::sleep_for(std::chrono::seconds(2));
+            h_.resume();
+        // });
     }
 
     CoTask::return_type await_resume() const noexcept { 
+        //  std::cout << "Type of result: " << typeid(a).name() << std::endl;
         // return h_.promise().value_;
-        return typename CoTask::return_type();
+        // return typename CoTask::return_type();
+        std::coroutine_handle<promise_type> originalHandle = std::coroutine_handle<promise_type>::from_address(h_.address());
+        auto a = originalHandle.promise().value_;
+        auto b =  a.has_value();
+        std::cout << "Type of result: " << typeid(a).name() << std::endl;
+        std::cout << "has_value : " << b << std::endl;
+        //  std::cout << "has_value : " << a.value() << std::endl;  //这个为什么是null
+        // return originalHandle.promise().value_.value_or( typename CoTask::return_type());
+        // return task_.value_;
+        // return 
     }
+
+    // CoTask &task_;
     std::coroutine_handle<> h_;
 };
 
@@ -62,6 +74,7 @@ struct Promise
 
     void return_value(return_type value){
         value_ = value;
+        std::cout<< "return_value : " << value_.value() << std::endl;
     }
 
     // 模板化，不然CoroutineTask<int>中没法co_awaitCoroutineTask<char> 
@@ -69,7 +82,13 @@ struct Promise
     initial_suspend_awaiter<CoTask2> await_transform(CoTask2 task){
         return initial_suspend_awaiter<CoTask2>();
     }
-    return_type  value_;
+
+    // // template<typename CoTask2>
+    // initial_suspend_awaiter<CoTask> await_transform(CoTask task){
+    //     return initial_suspend_awaiter<CoTask>();
+    // }
+    // return_type  value_;
+    std::optional<return_type> value_;
 };
 
 template <typename ReturnType>
@@ -80,11 +99,25 @@ struct CoroutineTask{
     
 };
 
+template <typename ReturnType>
+struct CoroutineTask2{
+    using return_type  = ReturnType;
+    using promise_type = Promise< CoroutineTask, ReturnType>;
+    std::coroutine_handle<promise_type> handle_;
+    
+};
+
+CoroutineTask<int *> three_coroutine(){
+    std::cout << "three_coroutine" << std::endl;
+    co_return nullptr;
+}
+
 CoroutineTask<int> second_coroutine(){
+    // co_await three_coroutine();
     std::cout << "second_coroutine" << std::endl;
     co_return 1;
 }
-CoroutineTask<char> first_coroutine(){
+CoroutineTask2<char> first_coroutine(){
     int a = co_await second_coroutine();
     std::cout << a << std::endl;
     co_return 'b';
@@ -98,5 +131,6 @@ CoroutineTask<char> first_coroutine(){
 
 int main(){
     // first_coroutine();
+    first_coroutine();
     getchar();
 }
